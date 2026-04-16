685ce480f6e5f20d581d13c9aa4e3daf4d728dd3
Soumu777-pro <soumyadeepdeep42@gmail.com>
2026-04-16 18:18:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,14 @@
 import json
-import os
 
 FILE = "data.json"
 
-
-# ================= LOAD =================
 def load():
-    if not os.path.exists(FILE):
-        return {"sessions": {}, "sudo": [], "settings": {}}
-
     try:
         with open(FILE, "r") as f:
             return json.load(f)
     except:
-        return {"sessions": {}, "sudo": [], "settings": {}}
+        return {"sessions": {}, "settings": {"auto": False, "delay": 3, "broadcast_msgs": []}, "sudo": []}
 
-
-# ================= SAVE =================
 def save(data):
     with open(FILE, "w") as f:
         json.dump(data, f, indent=2)
