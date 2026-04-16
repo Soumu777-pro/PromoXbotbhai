35e2cb289a35df57d33f308ba87e55bc17880d57
Soumu777-pro <soumyadeepdeep42@gmail.com>
2026-04-16 18:29:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,18 @@ def load():
         with open(FILE, "r") as f:
             return json.load(f)
     except:
-        return {"sessions": {}, "settings": {"auto": False, "delay": 3, "broadcast_msgs": []}, "sudo": []}
+        return {
+            "sessions": {},
+            "settings": {
+                "auto": False,
+                "delay": 3,
+                "dp": 0,
+                "max": 999,
+                "batch": False,
+                "broadcast_msgs": []
+            },
+            "sudo": []
+        }
 
 def save(data):
     with open(FILE, "w") as f:
